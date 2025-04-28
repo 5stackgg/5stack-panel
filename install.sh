e8549d7206b7895396815a5d2176525e0f36ec04
Luke Policinski <luke@lukepolo.com>
2025-04-28 13:49:42 -0400

    wip

--- install.sh
+++ install.sh
@@ -17,15 +17,14 @@ mkdir -p /opt/5stack/custom-plugins
 
 echo "Environment files setup complete"
 
-echo "Installing Kustomize ..."
 curl -s "https://raw.githubusercontent.com/kubernetes-sigs/kustomize/master/hack/install_kustomize.sh"  | bash
-
 
 curl -sfL https://get.k3s.io | sh -s - --disable=traefik
 
+output_redirect kubectl apply -f https://raw.githubusercontent.com/kubernetes/ingress-nginx/controller-v1.12.1/deploy/static/provider/baremetal/deploy.yaml
+
 source update.sh "$@"
 
-echo "Updating Node to host services"
-kubectl label node $(kubectl get nodes -o jsonpath='{.items[0].metadata.name}') 5stack-api=true 5stack-hasura=true 5stack-minio=true 5stack-timescaledb=true 5stack-redis=true 5stack-typesense=true 5stack-web=true
+output_redirect kubectl label node $(kubectl get nodes -o jsonpath='{.items[0].metadata.name}') 5stack-api=true 5stack-hasura=true 5stack-minio=true 5stack-timescaledb=true 5stack-redis=true 5stack-typesense=true 5stack-web=true
 
 echo "Installed 5Stack"

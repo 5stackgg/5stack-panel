36dbb7064b9dc694a45dd9bcf8ddc5dd3fddd0b9
Luke Policinski <luke@lukepolo.com>
2026-01-26 10:38:43 -0500

    wip

--- nvidia-drivers.sh
+++ nvidia-drivers.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 set -e
-apt install -y nvidia-container-toolkit nvidia-container-runtime cuda-drivers-fabricmanager-580 nvidia-headless-580-server-open nvidia-utils-580-server
+apt install -y nvidia-cuda-toolkit nvidia-container-toolkit nvidia-container-runtime cuda-drivers-fabricmanager-580 nvidia-headless-580-server-open nvidia-utils-580-server nvidia-driver-580-server-open
 
 kubectl create -f https://raw.githubusercontent.com/NVIDIA/k8s-device-plugin/v0.17.1/deployments/static/nvidia-device-plugin.yml

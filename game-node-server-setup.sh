43be3010c8ffb48fc5b321241228ebc878cdfe19
Luke Policinski <luke@lukepolo.com>
2026-04-29 10:31:52 -0400

    #build

--- game-node-server-setup.sh
+++ game-node-server-setup.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
-source setup-env.sh "$@"
+SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+source "$SCRIPT_DIR/utils/utils.sh" "$@"
 check_sudo
 
 if ! command -v jq &> /dev/null; then
